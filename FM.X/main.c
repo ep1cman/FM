20d2e04e48b843c0e6c982fa62dac92148400bb1
stuartrayner1994 <stuart.1994@hotmail.com>
2015-02-24 23:57:23 +0000

    LCD code moved to branch

--- FM.X/main.c
+++ FM.X/main.c
@@ -43,6 +43,28 @@
 #pragma config CP = OFF         // Code Protect 00000-03FFF (Program memory block (000000-003FFFh) not code-protected)
 
 int main(int argc, char** argv) {
+
+    TRISD = 0x00;
+    TRISF &= 0b11111100;
+
+    /* Turn on display */
+
+    LATF &= 0b00000000;
+    LATD = 0b10000000;
+    LATF |= 0b00000001;
+
+    /*Set DDRAM Address*/
+
+    LATF &= 0b00000000;
+    LATD = 0b10000000;
+    LATF |= 0b00000001;
+
+    /*Set Output data*/
+
+    LATF |= 0b00000010;
+    LATD = 0b00100011;
+    LATF |= 0b00000011;
+
     return (EXIT_SUCCESS);
 }
 
