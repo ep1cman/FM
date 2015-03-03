9f51a32b11c7a545226969a471dcc1b0a52cc8ef
stuartrayner1994 <stuart.1994@hotmail.com>
2015-03-03 00:01:56 +0000

    LCD Main Code
    Data for output still in need of loading, once ROM code number obtained.

--- FM.X/main.c
+++ FM.X/main.c
@@ -8,64 +8,61 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <xc.h>
 
-// #pragma config statements should precede project file includes.
-// Use project enums instead of #define for ON and OFF.
+/* Function Declaration */
+int LCD();
 
-// CONFIG1H
-#pragma config OSC = INTIO67       // Oscillator (External RC oscillator, port function on RA6)
-#pragma config FCMEN = OFF      // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor disabled)
-#pragma config IESO = OFF       // Internal External Switch Over Mode (Oscillator Switchover mode disabled)
+int main(int argc, char** argv) 
+{
+    int LCD();
+    return (EXIT_SUCCESS);
+}
 
-// CONFIG2L
-#pragma config PWRT = OFF       // Power Up Timer (PWRT disabled)
-#pragma config BOREN = SBORDIS  // Brown Out Detect (Brown-out Reset enabled in hardware only (SBOREN is disabled))
-#pragma config BORV = 3         // Brown Out Voltage (VBOR set to 2.1V)
+int LCD()
+{   
+ 
+    int ADD = 0b10000000;  /* Intial Segment Address */
+    int DATA[] = {/* load output data here */};
+    int LCD_Address(int sn , int *ADD);
+    int LCD_Data(int sn , int *DATA);
 
-// CONFIG2H
-#pragma config WDT = OFF        // Watchdog Timer (WDT disabled (control is placed on the SWDTEN bit))
-#pragma config WDTPS = 32768    // Watchdog Postscaler (1:32768)
-
-// CONFIG3L
-
-// CONFIG3H
-#pragma config CCP2MX = PORTC   // CCP2 Mux (CCP2 input/output is multiplexed with RC1)
-#pragma config LPT1OSC = OFF    // Low Power Timer1 Osc enable (Timer1 configured for higher power operation)
-#pragma config MCLRE = ON       // Master Clear Enable (MCLR pin enabled; RG5 input pin disabled)
-
-// CONFIG4L
-#pragma config STVREN = ON      // Stack Overflow Reset (Stack full/underflow will cause Reset)
-#pragma config XINST = OFF      // Extended Instruction Set Enable bit (Instruction set extension and Indexed Addressing mode disabled (Legacy mode))
-
-// CONFIG5L
-#pragma config CP = OFF         // Code Protect 00000-03FFF (Program memory block (000000-003FFFh) not code-protected)
-
-#define _XTAL_FREQ 8000000
-int main(int argc, char** argv) {
-
+    /* Initialise Ports */
     TRISD = 0x00;
     TRISF &= 0b11111100;
 
     /* Turn on display */
-
     LATF &= 0b00000000;
     LATD = 0b10000000;
     LATF |= 0b00000001;
 
-    /*Set DDRAM Address*/
-
+    /* Function Set */
     LATF &= 0b00000000;
-    LATD = 0b10000000;
+    LATD = 0b00111000;
     LATF |= 0b00000001;
+    
+    for (int sn = 0 ; sn < 32 ; sn++)
+    {
+    ADD = LCD_Address( sn , &ADD );
+    LCD_Data(sn , &DATA);
+    }
 
-    /*Set Output data*/
-
-    LATF |= 0b00000010;
-    LATD = 0b00100011;
-    LATF |= 0b00000011;
-
-    return (EXIT_SUCCESS);
 }
-
+int LCD_Address(int sn , int *ADD)
+{
+    /*Set DDRAM Address*/
+    LATF &= 0b00000000;
+    if (sn >= 16) *ADD |= 0b01000000; /* Start new line if top line full */
+    LATD = *ADD;
+    LATF |= 0b00000001;
+    *ADD++; /* Set next adress */
+    return 0;
+}
+int LCD_Data(int sn , int *DATA)
+{
+    /*Set Output data*/
+    LATF |= 0b00000010;
+    LATD = DATA[sn];
+    LATF |= 0b00000011;
+    return 0;
+}
