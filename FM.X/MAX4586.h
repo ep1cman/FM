d9d6095165a40618bc9fe0c330da0466fb3870ab
Sebastian Goscik <sebastian.goscik@live.co.uk>
2015-04-25 20:23:55 +0100

    Simplified Mux library
    Removed the switch case and error output

--- FM.X/MAX4586.h
+++ FM.X/MAX4586.h
@@ -11,12 +11,7 @@
 #define MAX4586_L_ADDR 0x6A
 #define MAX4586_R_ADDR 0x6E
 
-#define OUTPUT_1 0x01
-#define OUTPUT_2 0x02
-#define OUTPUT_3 0x04
-#define OUTPUT_4 0x08
-
-int selectOutput(int outputNumber);
+void selectOutput(int outputNumber);
 
 
 #endif	/* MAX4586_H */
