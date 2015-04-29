539579ed49945b6520392d7191de920774050f4f
Sebastian Goscik <sebastian.goscik@live.co.uk>
2015-04-29 16:31:45 +0100

    Fixed recorder library
    Phil should not code late at night...

--- FM.X/Recorder.c
+++ FM.X/Recorder.c
@@ -6,118 +6,120 @@
  */
 
 #include <i2c.h>
-#include <RECORDER.h>
+#include "Recorder.h"
+
+#define _XTAL_FREQ 1000000
 
 void startRecording(unsigned char fileNumber) {
-	IdleI2C;
-	StartI2C;
+	IdleI2C();
+	StartI2C();
 	WriteI2C(RECORDER_ADDR);
-	IdleI2C;
+	IdleI2C();
 	WriteI2C(0x01);
-	IdleI2C;
+	IdleI2C();
 	WriteI2C(fileNumber);
-	IdleI2C;
-	StopI2C;
+	IdleI2C();
+	StopI2C();
 
 	__delay_ms(500);
 
-	IdleI2C;
-	StartI2C;
-	Write12C(RECORDER_ADDR+1);
-	IdleI2C;
+	IdleI2C();
+	StartI2C();
+	WriteI2C(RECORDER_ADDR+1);
+	IdleI2C();
 	unsigned char rtn = ReadI2C();
-	IldeI2C();
+	IdleI2C();
 	NotAckI2C();
-	IdleI2C;
-	StopI2C;
+	IdleI2C();
+	StopI2C();
 }
 
 void stopRecording() {
-	IdleI2C;
-	StartI2C;
+	IdleI2C();
+	StartI2C();
 	WriteI2C(RECORDER_ADDR);
-	IdleI2C;
+	IdleI2C();
 	WriteI2C(0x02);
-	IdleI2C;
-	StopI2C;
+	IdleI2C();
+	StopI2C();
 
 	__delay_ms(500);
 
-	IdleI2C;
-	StartI2C;
-	Write12C(RECORDER_ADDR+1);
-	IdleI2C;
+	IdleI2C();
+	StartI2C();
+	WriteI2C(RECORDER_ADDR+1);
+	IdleI2C();
 	unsigned char rtn = ReadI2C();
-	IldeI2C();
+	IdleI2C();
 	NotAckI2C();
-	IdleI2C;
-	StopI2C;
+	IdleI2C();
+	StopI2C();
 }
 
 void playRecording(unsigned char fileNumber) {
-	IdleI2C;
-	StartI2C;
+	IdleI2C();
+	StartI2C();
 	WriteI2C(RECORDER_ADDR);
-	IdleI2C;
+	IdleI2C();
 	WriteI2C(0x04);
-	IdleI2C;
+	IdleI2C();
 	WriteI2C(fileNumber);
-	IdleI2C;
-	StopI2C;
+	IdleI2C();
+	StopI2C();
 
 	__delay_ms(500);
 
-	IdleI2C;
-	StartI2C;
-	Write12C(RECORDER_ADDR+1);
-	IdleI2C;
+	IdleI2C();
+	StartI2C();
+	WriteI2C(RECORDER_ADDR+1);
+	IdleI2C();
 	unsigned char rtn = ReadI2C();
-	IldeI2C();
+	IdleI2C();
 	NotAckI2C();
-	IdleI2C;
-	StopI2C;
+	IdleI2C();
+	StopI2C();
 }
 
 void pausePlayback() {
-	IdleI2C;
-	StartI2C;
+	IdleI2C();
+	StartI2C();
 	WriteI2C(RECORDER_ADDR);
-	IdleI2C;
+	IdleI2C();
 	WriteI2C(0x05);
-	IdleI2C;
-	StopI2C;
+	IdleI2C();
+	StopI2C();
 
 	__delay_ms(500);
 
-	IdleI2C;
-	StartI2C;
-	Write12C(RECORDER_ADDR+1);
-	IdleI2C;
+	IdleI2C();
+	StartI2C();
+	WriteI2C(RECORDER_ADDR+1);
+	IdleI2C();
 	unsigned char rtn = ReadI2C();
-	IldeI2C();
+	IdleI2C();
 	NotAckI2C();
-	IdleI2C;
-	StopI2C;
+	IdleI2C();
+	StopI2C();
 }
 
 void stopPlayback() {
-	IdleI2C;
-	StartI2C;
+	IdleI2C();
+	StartI2C();
 	WriteI2C(RECORDER_ADDR);
-	IdleI2C;
+	IdleI2C();
 	WriteI2C(0x06);
-	IdleI2C;
-	StopI2C;
+	IdleI2C();
+	StopI2C();
 
 	__delay_ms(500);
 
-	IdleI2C;
-	StartI2C;
-	Write12C(RECORDER_ADDR+1);
-	IdleI2C;
+	IdleI2C();
+	StartI2C();
+	WriteI2C(RECORDER_ADDR+1);
+	IdleI2C();
 	unsigned char rtn = ReadI2C();
-	IldeI2C();
+	IdleI2C();
 	NotAckI2C();
-	IdleI2C;
-	StopI2C;
+	IdleI2C();
+	StopI2C();
 }
