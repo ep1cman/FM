5c7838a460addcd5d7eb04454a028011231d0958
Sebastian Goscik <sebastian.goscik@live.co.uk>
2015-04-29 16:32:58 +0100

    Menu
    Added the menu to select modes and all features. Mostly implemented the recorder features until the PIC ran out of memory

--- FM.X/main.c
+++ FM.X/main.c
@@ -50,6 +50,8 @@
 #include "LCD.h"
 #include "DS3231.h"
 #include "MAX4586.h"
+#include "EEPROM.h"
+#include "Recorder.h"
 
 #define VOL_DOWN 0x01
 #define VOL_UP 0x02
@@ -67,7 +69,8 @@ unsigned char oldDebouncedState;	// Debounced state of the switches
 unsigned char State[MAX_CHECKS];	// Array that maintains bounce status
 unsigned char Index;	// Pointer into State
 
-enum menu {MAIN_MENU, REC, BT };
+enum menu {MENU, FM, BT, REC, PLAYBACK, PLAYING, OFF};
+enum dateSelect {DATE_SEL,MONTH_SEL,HOUR_SEL,MIN_SEL};
 
 unsigned char debounce()
 {
@@ -92,6 +95,7 @@ void main(int argc, char** argv) {
     //Initialise I2C
     OpenI2C(MASTER, SLEW_OFF);
     TRISC = 0b00011000;
+    LATC |= 0b00000100; //Turn on backlight
     TRISB = 0xFF;
     ADCON1 |= 0b1111;
     
@@ -100,104 +104,264 @@ void main(int argc, char** argv) {
     setVolume(12);
    
     char* buffer [10] = {0,0,0,0,0,0,0,0,0,0};
-    struct dateTime currentDateTime;
-    unsigned char menuChanged = 1;
-    enum menu menuState = MAIN_MENU;
+    struct dateTime scheduleSelect;
+    enum menu menuState = FM;
+    enum menu oldMenu = MENU;
     unsigned char preset;
+    unsigned char file = 1                           ;
     
     while(1)
     {
         unsigned char buttonState = debounce();
+        unsigned char buttonPressed = (buttonState>0);
         
         if ((oldDebouncedState & VOL_UP) && (oldDebouncedState & VOL_DOWN))
         {
-            menuChanged = 1;
-            menuState++;
-            if (menuState > BT) menuState = MAIN_MENU;
+            oldMenu = menuState;
+            menuState = MENU;
         }
         
         switch(menuState)
         {
-            case MAIN_MENU:
+            case MENU:
+                //Turn on LCD Backlight
+                LATC |= 0b00000100;
+                
+                writeToLCD(CLEAR_LCD, 'c');
+                printLCD("FM BT REC PB OFF");
+                setCursorLocation(0,1);
+    			printLCD((char *)"\x7F    \x7E    x    \x07");
+                writeToLCD(CURSOR_ON, 'c');
+                setCursorLocation(0,0);
+                enum menu selection = oldMenu;
+                while(menuState == MENU)
+                {
+                    buttonState = debounce();
+                    if (buttonState & PRESET_DOWN)
+                        selection--;
+                    else if (buttonState & FREQ_DOWN)
+                        selection++;
+                    else if (buttonState & FREQ_UP) 
+                        menuState = oldMenu;
+                    else if (buttonState & PRESET_UP)
+                        menuState = selection;
+                    
+                    if (selection==MENU) selection = OFF;
+                    if (selection>OFF) selection = FM;
+                    switch (selection)
+                    {
+                        case FM:
+                            setCursorLocation(0,0);
+                            break;
+                        case BT:
+                            setCursorLocation(3,0);
+                            break;
+                        case REC:
+                            setCursorLocation(6,0);
+                            break;
+                        case PLAYBACK:
+                            setCursorLocation(10,0);
+                            break;
+                        case OFF:
+                            setCursorLocation(13,0);
+                    }
+                }           
+                oldMenu = MENU;
+                writeToLCD(CURSOR_OFF, 'c');
+                break;
+            case FM:
                 //Redraw the screen edge only if the menu screen has changed
-    			if (menuChanged)
+    			if (oldMenu != menuState)
     			{
                     selectOutput(3);
-    				setCursorLocation(0, 0);
+                    writeToLCD(CLEAR_LCD, 'c');
     				writeToLCD(0x00, 'd'); //Up arrow
-                    char freqStr[16];
-    				sprintf(freqStr, "   %.1fMhz  ", ((float)getFrequency())/10);
-                    printLCD(freqStr);
     				setCursorLocation(0,1);
-    				writeToLCD(0x01, 'd'); //Down arrow
-    				printLCD((char *)"P- <<  >>  P+ ");
-    				menuChanged = 0;
-                    __delay_ms(500);
+    				printLCD((char *)"\x01P-  <<   >>  P+ ");
+                    setCursorLocation(5,0);
+    				oldMenu = menuState;
+                    buttonPressed = 1;
     			}
 
-			    //Volume buttons
+			    //Volume
                 if (buttonState & VOL_UP) volumeUp();
                 else if(buttonState & VOL_DOWN) volumeDown();
-
+                
+                //Seek
                 if ((oldDebouncedState & PRESET_DOWN) && (oldDebouncedState & FREQ_DOWN))
-                {
-                    seek('d');
-                    char freqStr[16];
-    				sprintf(freqStr, " %.1fMhz   ", ((float)getFrequency())/10);
-                    setCursorLocation(3,0);
-                    printLCD(freqStr);
-                }
-                
-                else if ((oldDebouncedState & PRESET_UP) && (oldDebouncedState & FREQ_UP))
-                {
-                    seek('u');
-                    char freqStr[16];
-    				sprintf(freqStr, " %.1fMhz   ", ((float)getFrequency())/10);
-                    setCursorLocation(3,0);
-                    printLCD(freqStr);
-                }
-                
+                    seek('d');                
+                else if((oldDebouncedState & PRESET_UP) && (oldDebouncedState & FREQ_UP))
+                    seek('u');   
+
                 //Preset Changed
 				else if (buttonState & PRESET_UP || buttonState & PRESET_DOWN)
 				{
-					if (buttonState & PRESET_UP) preset++;
-					else if (buttonState & PRESET_DOWN) preset--;
-					if (preset<1) preset = 9;
-				    if (preset>9) preset = 1;
+                    //Process Preset change
+                    if (buttonState & PRESET_UP) preset++;
+                    else if (buttonState & PRESET_DOWN) preset--;
 
-				    //Display selected preset
-				    setCursorLocation(3,0);
-					char presetStr[16];
-					sprintf(presetStr, "P%d:%.1fMhz  ", preset, ((float)getFrequency())/10);
-					printLCD(presetStr);
-				}
-                
-    			//Print the currently frequency
-    			else if (buttonState & FREQ_UP || buttonState & FREQ_DOWN)
+                    //Wait incase the second preset button is also pressed
+                    __delay_ms(500);
+                    buttonState = debounce();
+                   
+                    //If the other preset button is pressed, save preset
+                    if (buttonState & PRESET_UP || buttonState & PRESET_DOWN)
+                    {
+                        //Undo the preset change
+                        if (buttonState & PRESET_UP) preset++;
+                        else if (buttonState & PRESET_DOWN) preset--;
+                        
+                        //Save to EEPROM
+                        EEPROMWriteRegister(preset*2,getFrequency()>>8);
+                        EEPROMWriteRegister(preset*2+1,getFrequency()&0x00FF);
+                        printLCD("S");
+                    } else {
+                        //Loop preset at ends
+                        if (preset<1) preset = 9;
+                        if (preset>9) preset = 1;
+
+                        //Tune into preset
+                        unsigned int presetFreq = EEPROMReadRegister(preset*2) << 8;
+                        presetFreq |= EEPROMReadRegister(preset*2+1);
+                        tune(presetFreq);
+
+                        //Display selected preset
+                        char presetStr[16];
+                        sprintf(presetStr, "P%d:%.1fMHz   ", preset, ((float)getFrequency())/10);
+                        setCursorLocation(3,0);
+                        printLCD(presetStr);
+                        buttonPressed = 0;
+                    }
+                }
+
+    			//Manual frequency step
+    			else if(buttonState & FREQ_UP || buttonState & FREQ_DOWN)
 				{
                     if (buttonState & FREQ_UP) tune(getFrequency()+1);
                     if (buttonState & FREQ_DOWN) tune(getFrequency()-1);
-                    
-    				char freqStr[16];
-    				sprintf(freqStr, " %.1fMhz   ", ((float)getFrequency())/10);
+    			} 
+                
+                if (buttonPressed)
+                {
+                    char freqStr[16];
+    				sprintf(freqStr, "  %.1fMHz   ", ((float)getFrequency())/10);
                     setCursorLocation(3,0);
                     printLCD(freqStr);
-    			} 
+                }
                 break;
+                
             case BT:
-                if(menuChanged)
+                if(oldMenu != menuState)
                 {
                     selectOutput(0);
                     writeToLCD(CLEAR_LCD, 'c');
-                    setCursorLocation(0,0);
-                    printLCD("   Bluetooth");
+                    printLCD("   Bluetooth  \x02\x03");
                     setCursorLocation(0,1);
-                    printLCD("    Receiver");
-                    menuChanged = 0;
-                    __delay_ms(500);
+                    printLCD("    Receiver  \x04\x05");
+                    oldMenu = BT;
                 }
                 break;
-         }
+                
+            case REC:
+                if(oldMenu != menuState)
+                {
+                    writeToLCD(CLEAR_LCD, 'c');
+                    oldMenu = REC;
+                    getDateTime(&scheduleSelect);
+                }
+                
+                setCursorLocation(0,0);
+                char buffer[16];
+                sprintf(buffer, "  %02d/%02d %02d:%02d   ", scheduleSelect.date, scheduleSelect.month, scheduleSelect.hour, scheduleSelect.min);
+                printLCD(buffer);
+                setCursorLocation(0,1);
+                printLCD((char *)"\x7f    \x7E    \x2B    \x2D");
+                
+
+                break;
+            case PLAYBACK:
+                if(oldMenu != menuState)
+                {
+                    buffer[16];
+                    sprintf(buffer, "Recording No.", file);
+                    writeToLCD(CLEAR_LCD, 'c');
+                    printLCD(buffer);
+                    setCursorLocation(0,1);
+                    printLCD((char *)"\x7F    \x7E    x    \x07");
+                    oldMenu = PLAYBACK;
+                }
+                                
+                //Previous File
+                if (buttonState & PRESET_DOWN) file--;
+                
+                //Next file    
+                else if (buttonState & FREQ_DOWN) file ++;
+                
+                //Exit
+                else if (buttonState & FREQ_UP) menuState = MENU;
+                
+                //Play
+                else if(buttonState & PRESET_UP) 
+                {
+                    menuState = PLAYING; 
+                    playRecording(file);
+                }
+                
+                if (file>10) file =1;
+                if (file==0) file = 10;
+                
+                //Play
+                if (buttonState & PRESET_UP)
+                    continue;
+                
+                setCursorLocation(14,0);
+                char file_Str[4];
+                sprintf(file_Str, "%2d", file);
+                printLCD(file_Str);
+                
+                break;
+            
+            case PLAYING:
+                if(oldMenu != menuState)
+                {
+                    selectOutput(1);
+                    buffer[16];
+                    sprintf(buffer, "Playing File %d", file);
+                    writeToLCD(CLEAR_LCD, 'c');
+                    printLCD(buffer);
+                    setCursorLocation(0,1);
+                    printLCD((char *)"    P    \xFF     ");
+                    oldMenu = PLAYING;
+                }
+                
+                //Play/Pause  
+                if (buttonState & FREQ_DOWN) pausePlayback();
+                
+                //Stop
+                else if (buttonState & FREQ_UP) 
+                {
+                    stopPlayback();
+                    menuState = PLAYBACK;
+                }
+                
+                break;
+                
+            case OFF:
+                if(oldMenu != menuState) 
+                {
+                    selectOutput(5);
+                    LATC &= 0b11111011;
+                }
+                setCursorLocation(0,0);
+                struct dateTime currentDateTime;
+                getDateTime(&currentDateTime);
+                sprintf(buffer, "   %02d/%02d/20%02d   ", currentDateTime.date, currentDateTime.month, currentDateTime.year);
+                printLCD(buffer);
+                setCursorLocation(0,1);
+                sprintf(buffer, "      %02d:%02d     ", currentDateTime.hour, currentDateTime.min);
+                printLCD(buffer);
+                __delay_ms(500);
+        }
     }
 }
 
